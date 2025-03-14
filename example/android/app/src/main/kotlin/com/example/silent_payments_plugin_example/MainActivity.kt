1e50c3e97b27c6eda995804b2b4ca97d30f8fd0f
kent-3 <100624004+kent-3@users.noreply.github.com>
2025-03-14 19:40:01 -0400

    first commit

---
+++ example/android/app/src/main/kotlin/com/example/silent_payments_plugin_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.silent_payments_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/silent_payments_plugin.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../silent_payments_plugin.podspec for more information.
+#include "../../src/silent_payments_plugin.c"
