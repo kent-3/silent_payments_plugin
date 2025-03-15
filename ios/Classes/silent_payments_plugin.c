fcd56cf51922f85e87b534ca308be98f5305ca74
kent-3 <100624004+kent-3@users.noreply.github.com>
2025-03-14 20:01:03 -0400

    add Cargokit

--- ios/Classes/silent_payments_plugin.c
+++ ios/Classes/silent_payments_plugin.c
@@ -1,3 +1,1 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../silent_payments_plugin.podspec for more information.
-#include "../../src/silent_payments_plugin.c"
+// This is an empty file to force CocoaPods to create a framework.ent_payments_plugin.c"
